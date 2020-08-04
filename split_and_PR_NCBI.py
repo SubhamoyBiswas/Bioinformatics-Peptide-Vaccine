f8df264ffdb7055c9c50d9b2de9b9f5874deabbd
SubhamoyBiswas <56751752+SubhamoyBiswas@users.noreply.github.com>
2020-08-04 14:58:47 +0530

    Update split_and_PR_NCBI.py

--- split_and_PR_NCBI.py
+++ split_and_PR_NCBI.py
@@ -24,30 +24,29 @@ def retrieval():
     s1=''
     c, h, r, v, e, w=-1, 0, [], [], '', []
     while s!='':
-        s=s.replace('\n', '')
-        if s.isalpha():
-            lk=list(set(list(s)))
-            if sorted(lk)==['A', 'C', 'G', 'T']:
-                e+=s
-        else:
-            s=s.replace('>', '')
-            s=s.replace('\n', ' ')
-            r.append([])
-            c+=1
-            r[c].append(s)
+        if s[0]=='>':
+            r.append(s.split(' ', 1))
+            r[h][0]=r[h][0].replace('>', '')
+            
+            h+=1
             v.append(e)
             e=''
+        else:
+            e+=s
         s=f.readline()
         if s=='':
             v.append(e)
-    ec=v.count('')
-    for i in range(ec):
-        v.remove('')
+    v.remove('')
+
     a=''
     err, error=0, []
     p=len(v)
     for i in range(p):
         v[i]=v[i].replace('\n','') #replacing the newlines by empty strings
+        #v[i]=v[i][291:794]
+        #start_index=v[i].find('IRCI')
+        #end_index=v[i].find('AVSA')
+        #v[i]=v[i][start_index:(end_index+4)]
         k=len(v[i])
         v[i]=v[i].upper()
         for j in range(k):
@@ -166,29 +165,77 @@ def retrieval():
                 writer=csv.writer(csvFile)
                 for lp in range(w1-(window_s-1)):
                     stg=v[i][lp:(lp+window_s)]
-                    x1=y1=ux1=uy1=0
-                    for d1 in stg:
-                        if d1=='A':
-                            x1-=1
-                        elif d1=='G':
-                            x1+=1
-                        elif d1=='C':
-                            y1+=1
-                        elif d1=='T':
-                            y1-=1
-                        ux1+=x1
-                        uy1+=y1
-                    ux1, uy1=ux1/window_s, uy1/window_s
-                    gr1=math.sqrt(ux1*ux1 + uy1*uy1)
-                    r[i].append(gr1)
+                    a1=a2=a3=a4=a4=a5=a6=a7=a8=a9=a10=a11=a12=a13=a14=a15=a16=a17=a18=a19=a20=ua1=ua2=ua3=ua4=ua4=ua5=ua6=ua7=ua8=ua9=ua10=ua11=ua12=ua13=ua14=ua15=ua16=ua17=ua18=ua19=ua20=0
+                    for d in stg:
+                        if d=='A':
+                            a1+=1
+                        elif d=='R':
+                            a2+=1
+                        elif d=='N':
+                            a3+=1
+                        elif d=='D':
+                            a4+=1
+                        elif d=='C':
+                            a5+=1
+                        elif d=='Q':
+                            a6+=1
+                        elif d=='E':
+                            a7+=1
+                        elif d=='G':
+                            a8+=1
+                        elif d=='H':
+                            a9+=1
+                        elif d=='I':
+                            a10+=1
+                        elif d=='L':
+                            a11+=1
+                        elif d=='K':
+                            a12+=1
+                        elif d=='M':
+                            a13+=1
+                        elif d=='F':
+                            a14+=1
+                        elif d=='P':
+                            a15+=1
+                        elif d=='S':
+                            a16+=1
+                        elif d=='T':
+                            a17+=1
+                        elif d=='W':
+                            a18+=1
+                        elif d=='Y':
+                            a19+=1
+                        elif d=='V':
+                            a20+=1
+                        ua1+=a1
+                        ua2+=a2
+                        ua3+=a3
+                        ua4+=a4
+                        ua5+=a5
+                        ua6+=a6
+                        ua7+=a7
+                        ua8+=a8
+                        ua9+=a9
+                        ua10+=a10
+                        ua11+=a11
+                        ua12+=a12
+                        ua13+=a13
+                        ua14+=a14
+                        ua15+=a15
+                        ua16+=a16
+                        ua17+=a17
+                        ua18+=a18
+                        ua19+=a19
+                        ua20+=a20
+                    ua1, ua2, ua3, ua4, ua5, ua6, ua7, ua8, ua9, ua10, ua11, ua12, ua13, ua14, ua15, ua16, ua17, ua18, ua19, ua20= ua1/w1, ua2/w1, ua3/w1, ua4/w1, ua5/w1, ua6/w1, ua7/w1, ua8/w1, ua9/w1, ua10/w1, ua11/w1, ua12/w1, ua13/w1, ua14/w1, ua15/w1, ua16/w1, ua17/w1, ua18/w1, ua19/w1, ua20/w1
+                    pr=math.sqrt(ua1*ua1 + ua2*ua2 + ua3*ua3 + ua4*ua4 + ua5*ua5 + ua6*ua6 + ua7*ua7 + ua8*ua8 + ua9*ua9 + ua10*ua10 + ua11*ua11 + ua12*ua12 + ua13*ua13 + ua14*ua14 + ua15*ua15 + ua16*ua16 + ua17*ua17 + ua18*ua18 + ua19*ua19 + ua20*ua20)
+                    r[i].append(pr)
                 lis1=r[i][4:len(r[i])]
                 lis1=list(lis1)
                 dict1[f'{name}'].append(lis1)
                 writer.writerow(r[i])
             csvFile.close()
                 
-    w2.remove(0)
-    w.remove(0)
     for j1 in w:
         name=str(j1)
         pv=[]
@@ -206,16 +253,16 @@ def retrieval():
                 pv.append(len(rw1))
             writer.writerow(rw)
             pv2=[]
-            for i in range(len(pv)-(window_s1-1)):
-                pv1=pv[i:(i+window_s1)]
-                mean=sum(pv1)/window_s1
-                pv2.append(mean)
+            for i in range(len(pv)):
+                #pv1=pv[i:(i+window_s1)]
+                #mean=sum(pv1)/window_s1
+                pv2.append(pv[i])
         csvFile1.close()
         file_n=f'{out_path}/{name}/coord.csv'
         with open(file_n, 'a+', newline='') as csvFile:
             writer=csv.writer(csvFile)
             for i in range(len(pv2)):
-                writer.writerow([(i+1), pv2[i]])
+                writer.writerow([(i+1), pv[i]])
         csvFile.close()
     for j1 in w:
         name=str(j1)
@@ -269,3 +316,4 @@ tk.Button(win, width=12, text='Quit', command=win.destroy, foreground='brown', r
 tk.Button(win, width=12, text='Submit', command=retrieval, foreground='dark green', relief='ridge', font=('Segoe Script', '14'), highlightthickness=4, highlightbackground='green', highlightcolor='white').place(x=304.25, y=595)
 
 tk.mainloop()
+
